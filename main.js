d9e875fa066a61bc7b988eced927833147888c22
monStack38 <36056273+monStack38@users.noreply.github.com>
2022-08-10 07:42:28 +0800

    add open/close nav bar

--- main.js
+++ main.js
@@ -1,38 +1,55 @@
 // change nav style on scroll
 
-window.addEventListener('scroll', () => {
-    document.querySelector('nav').classList.toggle('window-scrolled', window.scrollY > 0);    
-})
-
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scrolled", window.scrollY > 0);
+});
 
 // CONTACT BUTTONS (circular text buttons)
 
-const textButtons = document.querySelectorAll('.contact__btn');
+const textButtons = document.querySelectorAll(".contact__btn");
 
-textButtons.forEach(textButton => {
-    let text = textButton.querySelector('p');
-    text.innerHTML = text.innerHTML.split('').map((character, index) => `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
-})
+textButtons.forEach((textButton) => {
+  let text = textButton.querySelector("p");
+  text.innerHTML = text.innerHTML
+    .split("")
+    .map(
+      (character, index) =>
+        `<span style="transform: rotate(${index * 12}deg)">${character}</span>`
+    )
+    .join("");
+});
 
 // SWIPER JS (GALLERY SECTION)
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 20,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    599: {
+      slidesPerView: 2,
+      spaceBetween: 40,
     },
-    breakpoints: {
-        599: {
-            slidesPerView: 2,
-            spaceBetween: 40
-        },
-        1023: {
-            slidesPerView: 3,
-            spaceBetween: 60
-        }
-    }
+    1023: {
+      slidesPerView: 3,
+      spaceBetween: 60,
+    },
+  },
 });
-  
 
+const nav = document.querySelector(".nav__links");
+const openNavBtn = document.querySelector("#nav__toggle-open");
+const closeNavBtn = document.querySelector("#nav__toggle-close");
+
+const openNav = () => {
+    nav.style.display = "flex";
+    openNavBtn.style.display = 'none';
+    closeNavBtn.style.display = 'inline';
+};
+
+openNavBtn.addEventListener("click", openNav);
